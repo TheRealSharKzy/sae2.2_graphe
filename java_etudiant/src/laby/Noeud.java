fedd8a24c2749d8fcbefc4416e25b0471e25d9bf
Thomas Boillot <thomas.boillot1@etu.univ-lorraine.fr>
2022-06-07 14:31:14 +0200

    noeud

--- java_etudiant/src/laby/Noeud.java
+++ java_etudiant/src/laby/Noeud.java
@@ -1,5 +1,30 @@
 package laby;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Noeud {
 
+    String nom;
+    List<Arc> adj;
+
+    public Noeud(String nom) {
+        this.nom = nom;
+        this.adj = new ArrayList<>();
+    }
+
+
+    public boolean equals(Noeud n) {
+        return nom.equals(n.getNom());
+    }
+
+    public  void ajouterArc(Noeud dest, double cout){
+        Arc arc = new Arc(dest,cout);
+        adj.add(arc);
+    }
+
+    public String getNom() {
+        return nom;
+    }
 }
