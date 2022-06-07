5d7fc62c9e4b28f29cda7de93e9892e2cc4a4eb5
fulacse <1335857832@qq.com>
2022-06-07 14:32:50 +0200

    constructeur de ARC

--- java_etudiant/src/laby/Arc.java
+++ java_etudiant/src/laby/Arc.java
@@ -1,4 +1,15 @@
 package laby;
 
 public class Arc {
+
+    private String dest;
+    private double cout;
+
+    public Arc(Noeud dest,double cout){
+        this.dest=dest.getNom();
+        this.cout=cout;
+        if(this.cout<0)
+            this.cout=0;
+    }
+
 }
