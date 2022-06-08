f26072c8843bdaae9941b6c25363ab5c9af59b1c
Thomas Boillot <thomas.boillot1@etu.univ-lorraine.fr>
2022-06-08 15:55:49 +0200

    Implémentation point fixe

---
+++ java_etudiant/src/BellmanFord.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class BellmanFord {
+
+    public Valeur resoudre(Graphe g, String depart) {
+        Valeur v = new Valeur();
+        List<Noeud> listeNoeud = ((GrapheListe) g).getEnsNoeuds();
+        for (Noeud n : listeNoeud) {
+            v.setValeur(n.getNom(), Double.MAX_VALUE);
+            v.setParent(n.getNom(), null);
+        }
+        v.setValeur(depart, 0);
+        for (int i = 1; i < listeNoeud.size(); i++) {
+            for (Arc s : g.suivants(depart)) {
+                if (v.getValeur(s.getDest()) > v.getValeur(listeNoeud.get(i).getNom()) + s.getCout()) {
+                    v.setValeur(s.getDest(), v.getValeur(listeNoeud.get(i).getNom()) + s.getCout());
+                    v.setParent(s.getDest(), listeNoeud.get(i).getNom());
+                }
+            }
+        }
+        return v;
+    }
+
+}
+
+
