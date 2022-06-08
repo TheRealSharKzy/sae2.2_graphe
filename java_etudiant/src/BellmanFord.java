67bb3646cac70ccf6cd4e78ee3157767d2023cf3
fulacse <1335857832@qq.com>
2022-06-08 16:04:58 +0200

    actuelle

--- java_etudiant/src/BellmanFord.java
+++ java_etudiant/src/BellmanFord.java
@@ -2,7 +2,7 @@ import java.util.List;
 
 public class BellmanFord {
 
-    public Valeur resoudre(Graphe g, String depart) {
+    public static Valeur resoudre(Graphe g, String depart) {
         Valeur v = new Valeur();
         List<Noeud> listeNoeud = ((GrapheListe) g).getEnsNoeuds();
         for (Noeud n : listeNoeud) {
@@ -11,7 +11,7 @@ public class BellmanFord {
         }
         v.setValeur(depart, 0);
         for (int i = 1; i < listeNoeud.size(); i++) {
-            for (Arc s : g.suivants(depart)) {
+            for (Arc s : g.suivants(listeNoeud.get(i).getNom())) {
                 if (v.getValeur(s.getDest()) > v.getValeur(listeNoeud.get(i).getNom()) + s.getCout()) {
                     v.setValeur(s.getDest(), v.getValeur(listeNoeud.get(i).getNom()) + s.getCout());
                     v.setParent(s.getDest(), listeNoeud.get(i).getNom());
