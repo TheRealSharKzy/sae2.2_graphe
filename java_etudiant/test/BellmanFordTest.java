543f35d75fa52c7e82f41da6f6edbedc1ff27798
fulacse <1335857832@qq.com>
2022-06-13 10:50:43 +0200

    BellmanFordTest

---
+++ java_etudiant/test/BellmanFordTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BellmanFordTest {
+    @Test
+    public void resoudre1() throws IOException {
+        GrapheListe graphe=new GrapheListe("./java_etudiant/documente/Graphe_test1.txt");
+        Valeur valeur=BellmanFord.resoudre(graphe,"A");
+        assertEquals(null,valeur.getParent("A"));
+        assertEquals(0,valeur.getValeur("A"));
+        assertEquals("A",valeur.getParent("B"));
+        assertEquals(1,valeur.getValeur("B"));
+        assertEquals("A",valeur.getParent("C"));
+        assertEquals(2,valeur.getValeur("C"));
+    }
+
+    @Test
+    public void resoudre2() throws IOException {
+        GrapheListe graphe=new GrapheListe("./java_etudiant/documente/Graphe_test2.txt");
+        Valeur valeur=BellmanFord.resoudre(graphe,"A");
+        assertEquals(null,valeur.getParent("A"));
+        assertEquals(0,valeur.getValeur("A"));
+        assertEquals("A",valeur.getParent("B"));
+        assertEquals(1,valeur.getValeur("B"));
+        assertEquals("B",valeur.getParent("C"));
+        assertEquals(4,valeur.getValeur("C"));
+    }
+}
