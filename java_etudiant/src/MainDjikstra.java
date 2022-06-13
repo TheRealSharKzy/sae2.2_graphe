b68ca47e561ceec2c2a0d6af1d00be1a67502474
Thomas Boillot <thomas.boillot1@etu.univ-lorraine.fr>
2022-06-13 14:00:54 +0200

    MainDjikstra

---
+++ java_etudiant/src/MainDjikstra.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class MainDjikstra {
+
+    public static void main(String[] args) throws IOException {
+
+        GrapheListe graphe = new GrapheListe("java_etudiant/documente/Graphe_exemple1.txt");
+        System.out.println(Djikstra.resoudre(graphe,"A"));
+
+    }
+
+
+}
