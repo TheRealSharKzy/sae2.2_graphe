f5edaa26d8e866d7613b989bbe3d69a8258c724f
Thomas Boillot <thomas.boillot1@etu.univ-lorraine.fr>
2022-06-14 16:07:29 +0200

    Changement Bellman Ford

--- java_etudiant/src/object/BellmanFord.java
+++ java_etudiant/src/object/BellmanFord.java
@@ -16,17 +16,20 @@ public class BellmanFord {
      * @return valeur du point final de l'algorithme
      */
     public static Valeur resoudre(Graphe g, String depart) {
-        Valeur v = new Valeur(depart);  //creer un valeur pour enregistre les poids
-        List<Noeud> listeNoeud = ((GrapheListe) g).getEnsNoeuds();  //creer un listre des noeuds
+        Valeur v = new Valeur(depart);  //cree une valeur pour enregistre les poids
+        List<Noeud> listeNoeud = ((GrapheListe) g).getEnsNoeuds();  //cree une liste des noeuds
         for (Noeud n : listeNoeud) {    //ajouter les noeuds dans valeur
             v.setValeur(n.getNom(), Double.MAX_VALUE);  //les poids des noeuds initialisent infini
             v.setParent(n.getNom(), null);  //les antécédants des noeuds initialisent null
         }
         v.setValeur(depart, 0); //le poid de antécédant de noeud de depart initialise 0;
-        for(int i=0;i<listeNoeud.size();i++){   //reperter pluseure fois
+        boolean changement = true;
+        while (changement){   //reperter pluseure fois
+            changement = false;
             for (Noeud n:listeNoeud) {  //pour chaque noeud
                 for (Arc arc : g.suivants(n.getNom())) {    //pour chaque arc
-                    if (v.getValeur(arc.getDest()) > v.getValeur(n.getNom()) + arc.getCout()) { //si le poid de le noeud est superrieus que le poid un chemin
+                    if (v.getValeur(arc.getDest()) > v.getValeur(n.getNom()) + arc.getCout()) { //si le poid du noeud est superieur au poid du chemin
+                        changement = true;
                         v.setValeur(arc.getDest(), v.getValeur(n.getNom()) + arc.getCout());    //changer le chemin
                         v.setParent(arc.getDest(), n.getNom()); //changer le parent
                     }
--- java_etudiant/src/object/Djikstra.java
+++ java_etudiant/src/object/Djikstra.java
@@ -21,16 +21,16 @@ public class Djikstra {
         Valeur valeur=new Valeur(depart);   //creer un valeur
         for(Noeud sommet:((GrapheListe)g).getEnsNoeuds()){  //ajouter chaque noeud dans valeur
             valeur.setValeur(sommet.getNom(),Double.MAX_VALUE); //un poid d'un noeud initialise infini
-            valeur.setParent(sommet.getNom(), null);    //un parent du poeud initialise infini
+            valeur.setParent(sommet.getNom(), null);    //un parent du poeud initialise null
             Q.add(sommet.getNom()); //ajouter le noeud dans Q
         }
         valeur.setValeur(depart,0); //le poid du depart initialise 0;
         while (Q.size()>0){ //tant que Q n'est pas vide
             String u=min(Q,valeur); //un noeud de q telle que son poid est minimale
             Q.remove(u);    //enlever u
-            for(Arc arc:g.suivants(u)){ //pour chaqu'arc de u
-                if(Q.contains(arc.getDest())){  //si un destination est dans Q
-                    double D=valeur.getValeur(u)+arc.getCout(); //le poid de'un chemin de le destination qui passe u
+            for(Arc arc:g.suivants(u)){ //pour chaque arc de u
+                if(Q.contains(arc.getDest())){  //si une destination est dans Q
+                    double D=valeur.getValeur(u)+arc.getCout(); //le poid d'un chemin de la destination qui passe u
                     if(D<valeur.getValeur(arc.getDest())){  //si le poid est moins lourd
                         valeur.setValeur(arc.getDest(),D);  //changer le poid
                         valeur.setParent(arc.getDest(),u);  //changer son parent
@@ -49,8 +49,8 @@ public class Djikstra {
      * @return  minimum
      */
     private static String min(List<String> list, Valeur valeur){
-        String res=list.get(0); //le premier élément de la listre
-        for(int i=1;i<list.size();i++){ //parcoure tout les rests
+        String res=list.get(0); //le premier élément de la liste
+        for(int i=1;i<list.size();i++){ //parcoure tous les restes
             if(valeur.getValeur(list.get(i))<valeur.getValeur(res)){    //si le poid est moins lourd
                 res= list.get(i);   //changer le resultat
             }
