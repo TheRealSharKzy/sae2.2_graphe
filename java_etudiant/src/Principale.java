9b244a4e063595c99a01ebd5b3bc8c3bb59aa2a7
fulacse <1335857832@qq.com>
2022-06-15 11:09:22 +0200

    refaire generationGraphe

--- java_etudiant/src/Principale.java
+++ java_etudiant/src/Principale.java
@@ -3,7 +3,9 @@ import object.GrapheListe;
 import object.Noeud;
 import object.generationGraphe;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,7 +13,12 @@ public class Principale {
 
     public static void main(String[] args) throws IOException {
         Graphe graphe= new GrapheListe(10);
-        System.out.println(((GrapheListe)graphe).toGraphviz());
+        File file=new File("java_etudiant/documente/GrapheGénération.puml");
+        PrintWriter writer=new PrintWriter(file);
+        writer.println("@startuml");
+        writer.println(((GrapheListe)graphe).toGraphviz());
+        writer.println("@enduml");
+        writer.close();
     }
 
 
--- java_etudiant/src/object/generationGraphe.java
+++ java_etudiant/src/object/generationGraphe.java
@@ -14,42 +14,35 @@ public class generationGraphe {
      * @return le graphe généré
      */
     public static Graphe generer(int taille){
-        List<Noeud> noeuds=new ArrayList<>();
-        for(int i=0;i<taille;i++){
-            noeuds.add(new Noeud(""+i));
+        List<Noeud> noeuds = new ArrayList<>();
+        for (int i = 0; i < taille; i++) {
+            noeuds.add(new Noeud("" + i));
         }
-        Graphe res=new GrapheListe(noeuds);
-        List<Noeud> noeudsIsole=new ArrayList<>(noeuds);
-        List<Noeud> noeudsChemin=new ArrayList<>();
-        Noeud noeud=noeudsIsole.get((int)(Math.random()*noeudsIsole.size()));
-        noeudsIsole.remove(noeud);
-        noeudsChemin.add(noeud);
-        for(int i=0;i<taille-1;i++){
-            noeud=noeudsIsole.get((int)(Math.random()* noeudsIsole.size()));
-            noeudsIsole.remove(noeud);
-            ((GrapheListe)res).ajouterArc(noeudsChemin.get(noeudsChemin.size()-1).getNom(),noeud.getNom(),(int)(Math.random()*100));
-            noeudsChemin.add(noeud);
-        }
-        ((GrapheListe)res).ajouterArc(noeud.getNom(),noeudsChemin.get(0).getNom(),(int)(Math.random()*100));
-        for(int i=0;i<=((int)(Math.random()* noeudsChemin.size()));i++){
-            noeud=noeudsChemin.get((int)(noeudsChemin.size()*Math.random()));
-            for(int j=0;j<((int)(Math.random()* noeudsChemin.size()));j++){
-                Noeud noeudDest=noeudsChemin.get(((int)(noeudsChemin.size()*Math.random())));
-                if(noeudDest==noeud){
-                    j--;
-                }else {
-                    boolean contient=false;
-                    for(Arc arc:noeud.getAdj()){
-                        if(arc.getDest().equals(noeudDest.getNom())){
-                            contient=true;
-                            break;
-                        }
-                    }
-                    if(!contient){
-                        ((GrapheListe)res).ajouterArc(noeud.getNom(),noeudDest.getNom(),(int)(Math.random()*100));
-                    }
-                }
+        Graphe res = new GrapheListe(noeuds);
+        List<Noeud> noeudsChemein = new ArrayList<>();
+        List<Noeud> noeudsIsole = new ArrayList<>(noeuds);
+        while (noeudsIsole.size() > 0) {
+            int nbNoeudTrait = 1 + (int) (Math.random() * noeudsIsole.size());
+            List<Noeud> trait = new ArrayList<>();
+            for (int i = 0; i < nbNoeudTrait; i++) {
+                Noeud noeud=noeudsIsole.get((int) (Math.random() * noeudsIsole.size()));
+                trait.add(noeud);
+                noeudsIsole.remove(noeud);
             }
+            for (int i = 0; i < trait.size() - 1; i++) {
+                ((GrapheListe) res).ajouterArc(trait.get(i).getNom(), trait.get(i + 1).getNom(), (int) (Math.random() * 100));
+            }
+            if (noeudsChemein.size() == 0) {
+                if(trait.size()>1){
+                    ((GrapheListe) res).ajouterArc(trait.get(trait.size() - 1).getNom(), trait.get(0).getNom(), (int) (Math.random() * 100));
+                }
+            } else {
+                Noeud noeud1 = noeudsChemein.get((int) (Math.random() * noeudsChemein.size()));
+                Noeud noeud2 = noeudsChemein.get((int) (Math.random() * noeudsChemein.size()));
+                ((GrapheListe) res).ajouterArc(noeud1.getNom(), trait.get(0).getNom(), (int) (Math.random() * 100));
+                ((GrapheListe) res).ajouterArc(trait.get(trait.size() - 1).getNom(), noeud2.getNom(), (int) (Math.random() * 100));
+            }
+            noeudsChemein.addAll(trait);
         }
         return res;
     }
